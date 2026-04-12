05422d51a60cfd53112cb4fbaaf5b88baabe85b7
Sleepingoff <justjh30@gmail.com>
2026-04-12 03:55:09 +0000

    design: 토큰 가공 결과

---
+++ src/init/styles/tokens.ts
@@ -0,0 +1,210 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const basicColorBlue50 = "#eff6ff";
+export const basicColorBlue100 = "#dbeafe";
+export const basicColorBlue200 = "#bfdbfe";
+export const basicColorBlue300 = "#93c5fd";
+export const basicColorBlue400 = "#60a5fa";
+export const basicColorBlue500 = "#3b82f6";
+export const basicColorBlue600 = "#2563eb";
+export const basicColorBlue700 = "#1d4ed8";
+export const basicColorBlue800 = "#1e40af";
+export const basicColorBlue900 = "#1e3a8a";
+export const basicColorBlue950 = "#172554";
+export const basicColorBluegrey50 = "#f5f7fa";
+export const basicColorBluegrey100 = "#f0f2f5";
+export const basicColorBluegrey200 = "#e1e3e6";
+export const basicColorBluegrey300 = "#d2d4d6";
+export const basicColorBluegrey400 = "#9ea1a5";
+export const basicColorBluegrey500 = "#717376";
+export const basicColorBluegrey600 = "#535557";
+export const basicColorBluegrey700 = "#404143";
+export const basicColorBluegrey800 = "#313233";
+export const basicColorBluegrey900 = "#19191a";
+export const basicColorBluegrey950 = "#0a0a0a";
+export const basicColorGrey50 = "#fafafa";
+export const basicColorGrey100 = "#f5f5f5";
+export const basicColorGrey200 = "#e6e6e6";
+export const basicColorGrey300 = "#d6d6d6";
+export const basicColorGrey400 = "#a5a5a5";
+export const basicColorGrey500 = "#767676";
+export const basicColorGrey600 = "#575757";
+export const basicColorGrey700 = "#434343";
+export const basicColorGrey800 = "#333333";
+export const basicColorGrey900 = "#1a1a1a";
+export const basicColorGrey950 = "#0a0a0a";
+export const basicColorRed50 = "#fff0ef";
+export const basicColorRed100 = "#fededb";
+export const basicColorRed200 = "#fec4bf";
+export const basicColorRed300 = "#fd9c93";
+export const basicColorRed400 = "#fa6d60";
+export const basicColorRed500 = "#f64b3b";
+export const basicColorRed600 = "#eb3525";
+export const basicColorRed700 = "#d82d1d";
+export const basicColorRed800 = "#af2a1e";
+export const basicColorRed900 = "#8a271e";
+export const basicColorRed950 = "#541c17";
+export const basicColorGreen50 = "#f2ffef";
+export const basicColorGreen100 = "#e3fedb";
+export const basicColorGreen200 = "#cdfebf";
+export const basicColorGreen300 = "#aafd93";
+export const basicColorGreen400 = "#81fa60";
+export const basicColorGreen500 = "#64f63b";
+export const basicColorGreen600 = "#50eb25";
+export const basicColorGreen700 = "#46d81d";
+export const basicColorGreen800 = "#3daf1e";
+export const basicColorGreen900 = "#358a1e";
+export const basicColorGreen950 = "#245417";
+export const basicColorPurple50 = "#faefff";
+export const basicColorPurple100 = "#f3dbfe";
+export const basicColorPurple200 = "#ebbffe";
+export const basicColorPurple300 = "#dd93fd";
+export const basicColorPurple400 = "#cc60fa";
+export const basicColorPurple500 = "#be3bf6";
+export const basicColorPurple600 = "#b025eb";
+export const basicColorPurple700 = "#a01dd8";
+export const basicColorPurple800 = "#831eaf";
+export const basicColorPurple900 = "#6a1e8a";
+export const basicColorPurple950 = "#421754";
+export const basicColorYellow50 = "#ffffef";
+export const basicColorYellow100 = "#fefedb";
+export const basicColorYellow200 = "#fefebf";
+export const basicColorYellow300 = "#fdfd93";
+export const basicColorYellow400 = "#fafa60";
+export const basicColorYellow500 = "#f6f63b";
+export const basicColorYellow600 = "#ebeb25";
+export const basicColorYellow700 = "#d8d81d";
+export const basicColorYellow800 = "#afaf1e";
+export const basicColorYellow900 = "#8a8a1e";
+export const basicColorYellow950 = "#545417";
+export const basicColorOrange50 = "#fff7ef";
+export const basicColorOrange100 = "#feecdb";
+export const basicColorOrange200 = "#fedebf";
+export const basicColorOrange300 = "#fdc893";
+export const basicColorOrange400 = "#faad60";
+export const basicColorOrange500 = "#f6983b";
+export const basicColorOrange600 = "#eb8825";
+export const basicColorOrange700 = "#d87a1d";
+export const basicColorOrange800 = "#af661e";
+export const basicColorOrange900 = "#8a541e";
+export const basicColorOrange950 = "#543517";
+export const basicColorWhiteBase = "#ffffff";
+export const basicColorBlackBase = "#000000";
+export const brandColorPrimary = "#3b82f6";
+export const brandColorSecondary = "#bfdbfe";
+export const typoColorPrimary = "#333333";
+export const typoColorSecondary = "#434343";
+export const typoColorPlaceholder = "#a5a5a5";
+export const typoColorSub1 = "#575757";
+export const typoColorRed = "#f64b3b";
+export const typoColorGreen = "#3daf1e";
+export const typoColorBlue = "#3b82f6";
+export const typoColorYellow = "#afaf1e";
+export const typoColorPurple = "#a01dd8";
+export const typoColorOrange = "#eb8825";
+export const typoColorSub2 = "#767676";
+export const spacingSpacing8 = 8;
+export const spacingSpacing10 = 10;
+export const spacingSpacing12 = 12;
+export const spacingSpaicng14 = 14;
+export const spacingSpacing16 = 16;
+export const spacingSpacing24 = 24;
+export const spacingSpacing30 = 30;
+export const spacingSpacing20 = 20;
+export const radiusRadius8 = 8;
+export const radiusRadius12 = 12;
+export const radiusRadius16 = 16;
+export const radiusRadius20 = 20;
+export const typographyPrimitiveFontSize10 = 10;
+export const typographyPrimitiveFontSize12 = 12;
+export const typographyPrimitiveFontSize14 = 14;
+export const typographyPrimitiveFontSize16 = 16;
+export const typographyPrimitiveFontSize18 = 18;
+export const typographyPrimitiveFontSize20 = 20;
+export const typographyPrimitiveLineHeight12 = 12;
+export const typographyPrimitiveLineHeight18 = 18;
+export const typographyPrimitiveLineHeight20 = 20;
+export const typographyPrimitiveLineHeight22 = 22;
+export const typographyPrimitiveLineHeight24 = 24;
+export const typographyPrimitiveLetterSpacing0 = 0;
+export const typographyPrimitiveLetterSpacing2 = 0.03999999910593033;
+export const typographyPrimitiveFontWeightBold = "Bold";
+export const typographyPrimitiveFontWeightSemibold = "SemiBold";
+export const typographyPrimitiveFontWeightMedium = "Medium";
+export const typographyPrimitiveFontWeightRegular = "Regular";
+export const typographyPrimitiveLineHeight14 = 14;
+export const typographyPrimitiveLineHeight16 = 16;
+export const backgroundColorPrimary = "#f5f7fa";
+export const backgroundColorBlue = "#eff6ff";
+export const backgroundColorRed = "#fff0ef";
+export const backgroundColorGreen = "#f2ffef";
+export const backgroundColorYellow = "#fefedb";
+export const backgroundColorPurple = "#faefff";
+export const backgroundColorOrange = "#feecdb";
+export const backgroundColorWhite = "#ffffff";
+export const buttonColorCta = "#3b82f6";
+export const buttonColorSecondary = "#f0f2f5";
+export const buttonColorUnavailable = "#d2d4d6";
+export const borderDividerPrimary = "#e6e6e6";
+export const borderDividerError = "#f64b3b";
+export const dimOverlayDefault = "#000000";
+export const dimOverlayLight = "#000000";
+export const shadowShadowSmColor = "#0f172a";
+export const shadowShadowSmY = 4;
+export const shadowShadowSmBlur = 12;
+export const shadowShadowMdColor = "#000000";
+export const shadowShadowMdY = 10;
+export const shadowShadowMdBlur = 28;
+export const stylesTextH1 = "700 20px/24 Pretendard"; // 앱타이틀, 온보딩타이틀, 바텀모달타이틀
+export const stylesTextH2 = "700 18px/24 Pretendard"; // 매장명, 공지 상페 타이틀, 피커숫자
+export const stylesTextB16 = "700 16px/22 Pretendard"; // 근무카드시간, 페이지 헤더타이틀, 스케줄 시간표시
+export const stylesTextM16 = "500 16px/20 Pretendard"; // CTA 버튼, 모달 본문, 로그인 서브 문구, 피커 단위
+// 메인 섹션 헤딩 "근무 일정", "공지사항", "근무자 리스트"
+// 공지 리스트(14px) 타이틀 "[필독] 이번 주말..."
+// 공지 작성 헤더 텍스트 버튼 "등록"
+export const stylesTextB14 = "700 14px/18 Pretendard";
+// 마이페이지 텍스트 카드 "프로필 정보", "알림 설정", "로그아웃"
+// 앱정보 항목 "개인정보 처리방침", "버전"
+// 알림 설정 항목 "공지 등록", "스케줄 변경", "근무 하루전"
+// 매장 가입 "나날이 관리자"
+export const stylesTextSb14 = "600 14px/16 Pretendard";
+// 텍스트필드 플레이스홀더
+// 회원가입 필드 라벨
+// 공지 상세 본문 텍스트
+// 직급 표시 "알바" / 시간 정보 "(총 18시간)"
+// 인증 버튼
+export const stylesTextM14 = "500 14px/16 Pretendard";
+export const stylesTextR14 = "400 14px/16 Pretendard"; // 필드라벨, 세그먼트, 탭(일간/주간)
+// 공지작성 본문 영역
+// 메모 작성 영역
+// 약관 전문 표시 영역
+export const stylesTextR14L = "400 14px/24 Pretendard";
+export const stylesTextB12 = "700 12px/14 Pretendard"; // 메인 홈 공지카드 타이틀
+// 근무카드 총시간
+// 로그인 페이지 링크문구 "회원가입", "간편로그인", "비밀번호찾기"
+// 인증타이머
+// 인증오류
+// 마이페이지 섹션 라벨
+export const stylesTextM12 = "500 12px/14 Pretendard";
+// 메인 공지카드 서브텍스트
+// 드롭다운 리스트
+export const stylesTextR12 = "400 12px/14 Pretendard";
+// 하단 내비라벨
+// 근무상태뱃지
+// 근무카드 날짜
+export const stylesTextMicro = "500 10px/12 Pretendard";
+export const stylesGrid나날이Grid = {
+  layoutGrids: [
+    {
+      pattern: "COLUMNS",
+      visible: true,
+      color: "#ff00001a",
+      alignment: "STRETCH",
+      gutterSize: 20,
+      offset: 16,
+      count: 4,
+    },
+  ],
+};
