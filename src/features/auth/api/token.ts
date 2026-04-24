3282a3447d56eee087dc2e29a4f9a22e79220ec2
SleepingOff <justjh30@gmail.com>
2026-04-24 13:37:11 +0900

    feat: auth 관련 api 구현 및 model 정의

---
+++ src/features/auth/api/token.ts
@@ -0,0 +1,6 @@
+import { apiClient } from '@/src/shared/api/api';
+import { RefreshTokenRequest } from '../model/token';
+
+export async function refreshToken(data: RefreshTokenRequest) {
+  return await apiClient.post('/auth/refresh', data);
+}
