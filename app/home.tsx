8f00d4eb57b0335c59502e0d8c9a0d7afb47f533
SleepingOff <justjh30@gmail.com>
2026-04-29 12:41:56 +0900

    fix: 조건문 내용 변경

--- app/home.tsx
+++ app/home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    if (!user) {
+    if (!user.email) {
       router.push('/auth');
     } else {
       router.replace('/store');
