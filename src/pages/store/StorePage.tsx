28c73f199ff86f2ef60a8e2eea89d0e162cb411f
SleepingOff <justjh30@gmail.com>
2026-04-29 12:38:02 +0900

    feat: 임시 로그아웃 기능 구현

--- src/pages/store/StorePage.tsx
+++ src/pages/store/StorePage.tsx
@@ -1,12 +1,27 @@
 import { useRouter } from 'expo-router';
 import { Pressable, Text } from 'react-native';
 
+import useUser from '@/src/features/user/lib/useUser';
+
 export default function StorePage() {
   const router = useRouter();
 
+  const user = useUser();
+
+  const handleLogout = () => {
+    const { clearUser } = user;
+    clearUser();
+    router.replace('/');
+  };
+
   return (
-    <Pressable onPress={() => router.push('/0')}>
-      <Text>go to store</Text>
-    </Pressable>
+    <>
+      <Pressable onPress={() => router.push('/0')}>
+        <Text>go to store</Text>
+      </Pressable>
+      <Pressable onPress={handleLogout}>
+        <Text>로그아웃</Text>
+      </Pressable>
+    </>
   );
 }
