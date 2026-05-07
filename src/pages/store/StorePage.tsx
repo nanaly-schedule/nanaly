9fd5f5e50d6d99a2d788ddcd945b43ee87cf9f18
SleepingOff <justjh30@gmail.com>
2026-05-07 12:07:22 +0900

    feat: 위젯으로 페이지 생성

--- src/pages/store/StorePage.tsx
+++ src/pages/store/StorePage.tsx
@@ -6,8 +6,16 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { deleteUser } from '@/src/features/auth/api/sign';
 import DeleteAccountModal from '@/src/features/auth/ui/DeleteAccountModal';
 import useUser from '@/src/features/user/lib/useUser';
-import { spacingSpacing12, typoColorRed } from '@/src/init/styles/tokens';
+import {
+  spacingSpacing12,
+  spacingSpacing16,
+  spacingSpaicng14,
+  typoColorRed,
+} from '@/src/init/styles/tokens';
 import NText from '@/src/shared/ui/NText';
+import EmptyHeader from '@/src/widgets/shared/EmptyHeader';
+import SectionHeader from '@/src/widgets/store/SectionHeader';
+import StoreCreateBtn from '@/src/widgets/store/StoreCreateBtn';
 
 export default function StorePage() {
   const router = useRouter();
@@ -15,10 +23,10 @@ export default function StorePage() {
   const [isDeleteAccountModalVisible, setIsDeleteAccountModalVisible] =
     useState(false);
 
-  const handleLogout = () => {
+  const handleLogout = async () => {
     const { clearUser } = user;
-    clearUser();
-    router.replace('/');
+    await clearUser();
+    router.replace('/auth');
   };
 
   const handleDeleteUser = async () => {
@@ -26,7 +34,7 @@ export default function StorePage() {
       await deleteUser();
 
       const { clearUser } = user;
-      clearUser();
+      await clearUser();
 
       router.replace('/');
     } catch (error) {
@@ -40,10 +48,17 @@ export default function StorePage() {
   //계정 삭제 버튼 관련 스타일은 추후 해당 페이지로 옮기면서 별도 스타일로 분리할 예정
   //이때, View 스타일의 flex : 1 또한 같이 옮겨야 함!
   return (
-    <View style={{ flex: 1 }}>
-      <Pressable onPress={() => router.push('/0')}>
-        <Text>go to store</Text>
-      </Pressable>
+    <View style={{ flex: 1, marginHorizontal: spacingSpacing16 }}>
+      <EmptyHeader />
+      <SectionHeader
+        title="어떻게 시작할까요?"
+        content="매장을 직접 만들거나 초대링크로 참여할 수 있어요"
+        style={{ marginTop: spacingSpaicng14 }}
+      />
+      <StoreCreateBtn
+        onCreateStore={() => router.push('/store/create/step1')}
+        onJoinStore={() => router.push('/store/join')}
+      />
       <Pressable onPress={handleLogout}>
         <Text>로그아웃</Text>
       </Pressable>
