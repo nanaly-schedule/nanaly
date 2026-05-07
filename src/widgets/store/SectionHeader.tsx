fe028e42d78e86cd70aa030eb397add979874497
SleepingOff <justjh30@gmail.com>
2026-05-07 12:05:55 +0900

    feat: section header 위젯 구현

---
+++ src/widgets/store/SectionHeader.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, View, ViewStyle } from 'react-native';
+
+import { spacingSpacing12, spacingSpacing30 } from '@/src/init/styles/tokens';
+import NText from '@/src/shared/ui/NText';
+
+interface SectionHeaderProps {
+  title: string;
+  content: string;
+  style?: ViewStyle;
+}
+
+export default function SectionHeader({
+  title,
+  content,
+  style,
+}: SectionHeaderProps) {
+  return (
+    <View style={[styles.headerContainer, style]}>
+      <NText variant="h1">{title}</NText>
+      <NText variant="m16">{content}</NText>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    gap: spacingSpacing12,
+    marginBottom: spacingSpacing30,
+  },
+});
