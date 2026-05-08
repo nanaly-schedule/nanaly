1e0fd21a0ae4cd1d34731b76615207a948ddb128
SleepingOff <justjh30@gmail.com>
2026-05-08 15:57:06 +0900

    feat: 매장 crud api 생성

--- src/features/store/model/store.ts
+++ src/features/store/model/store.ts
@@ -5,3 +5,8 @@ export interface StoresRequest {
   businessName: string; //'버거킹';
   storeName: string; //'버거킹 인하대역점';
 }
+
+export interface StoreInfoRequest {
+  storeName: string; //'버거킹 인하대역점';
+  phone: string; //'02-1234-5678';
+}
