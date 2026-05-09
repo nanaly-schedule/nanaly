86521d86bf0dbfc0fb0b323c90a9c2c5cf5e4e1d
SleepingOff <justjh30@gmail.com>
2026-05-09 12:11:08 +0900

    feat: 타이틀 버튼 위젯 분리

---
+++ src/widgets/store/TitleButton.tsx
@@ -0,0 +1,56 @@
+import {
+  GestureResponderEvent,
+  Pressable,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import {
+  backgroundColorWhite,
+  radiusRadius8,
+  spacingSpacing8,
+  typoColorPrimary,
+  typoColorSub1,
+} from '@/src/init/styles/tokens';
+import RightArrowIcon from '@/src/shared/assets/RightArrowIcon';
+import NText from '@/src/shared/ui/NText';
+
+interface TitleButtonProps {
+  title: string;
+  isPlaceholder?: boolean;
+  showIcon?: boolean;
+  onPress: (e: GestureResponderEvent) => void;
+}
+
+export default function TitleButton({
+  title,
+  onPress,
+  isPlaceholder = false,
+  showIcon = true,
+}: TitleButtonProps) {
+  return (
+    <Pressable style={styles.container} onPress={onPress}>
+      <NText
+        variant="m14"
+        style={{ color: isPlaceholder ? typoColorSub1 : typoColorPrimary }}
+      >
+        {title}
+      </NText>
+      <View style={{ paddingHorizontal: 6 }}>
+        {showIcon && <RightArrowIcon size={10} color={typoColorPrimary} />}
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: radiusRadius8,
+    paddingHorizontal: spacingSpacing8,
+    backgroundColor: backgroundColorWhite,
+    height: 46,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
