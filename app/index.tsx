4ccbdfb5466267594705f3a000485e55e27f398e
SleepingOff <justjh30@gmail.com>
2026-05-11 14:04:23 +0900

    feat: 리다이렉트 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,31 @@
-import { Redirect } from 'expo-router';
+import { useRouter } from 'expo-router';
+import { useEffect } from 'react';
+import { View } from 'react-native';
+
+import { getMyStore } from '@/src/features/store/api/store';
 
 export default function Home() {
-  return <Redirect href={'/store'} />;
+  const router = useRouter();
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const { data: myStores } = await getMyStore();
+        if (myStores[0]) {
+          const [{ permissions, storeId, role }] = myStores;
+          router.push({
+            pathname: '/[storeId]',
+            params: { storeId, role, permissions },
+          });
+        } else {
+          router.push('/store');
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (_) {
+        router.replace('/store');
+      }
+    };
+
+    fetch();
+  }, [router]);
+  return <View />;
 }
