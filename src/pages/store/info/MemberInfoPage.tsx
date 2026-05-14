a6ec6132020f9c044bc05ecd47b736eb54ee38bc
SleepingOff <justjh30@gmail.com>
2026-05-14 14:19:44 +0900

    feat: 근무자 정보 관련 페이지 라우터 추가

---
+++ src/pages/store/info/MemberInfoPage.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+import PageLayout from '@/src/shared/ui/PageLayout';
+
+export default function MemberInfoPage() {
+  return (
+    <PageLayout title="근무자 정보" showCheckIcon>
+      <View></View>
+    </PageLayout>
+  );
+}
