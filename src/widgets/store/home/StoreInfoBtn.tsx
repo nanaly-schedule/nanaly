5c3e0640a6a90163ce8adcaa2e8f4c21c407b9ea
SleepingOff <justjh30@gmail.com>
2026-05-14 12:50:17 +0900

    ref: 경로 변경

--- src/widgets/store/home/StoreInfoBtn.tsx
+++ src/widgets/store/home/StoreInfoBtn.tsx
@@ -30,6 +30,7 @@ export default function StoreInfoBtn() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     flexDirection: 'row',
     borderRadius: radiusRadius12,
     paddingHorizontal: spacingSpacing8,
