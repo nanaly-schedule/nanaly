f4ef83b4cc26c36289352dc4319e809727c94c02
yeji <skql4128@naver.com>
2026-07-18 05:01:20 +0900

    fix: 로그인 페이지 두번 이동 에러 수정

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,44 @@
 import { useRouter } from 'expo-router';
 import { useEffect } from 'react';
-import { View } from 'react-native';
 
+import { getAccessToken } from '@/src/features/auth/lib/storage';
 import { getMyStore } from '@/src/features/store/api/store';
-import SignInPage from '@/src/pages/auth/SignInPage';
 
 export default function Home() {
   const router = useRouter();
+
   useEffect(() => {
     const fetch = async () => {
-      const { data: myStores } = await getMyStore();
-      if (myStores[0]) {
-        const [{ permissions, storeId, role, storeName }] = myStores;
-        router.replace({
-          pathname: '/[storeId]',
-          params: {
-            storeId,
-            role,
-            permissions,
-            displayStoreName: storeName,
-          },
-        });
-      } else {
-        router.replace('/store');
+      try {
+        const accessToken = await getAccessToken();
+
+        if (!accessToken) {
+          router.replace('/auth');
+          return;
+        }
+
+        const { data: myStores } = await getMyStore();
+        if (myStores[0]) {
+          const [{ permissions, storeId, role, storeName }] = myStores;
+          router.replace({
+            pathname: '/[storeId]',
+            params: {
+              storeId,
+              role,
+              permissions,
+              displayStoreName: storeName,
+            },
+          });
+        } else {
+          router.replace('/store');
+        }
+      } catch {
+        router.replace('/auth');
       }
     };
 
     fetch();
   }, [router]);
-  return <SignInPage />;
+
+  return null;
 }
