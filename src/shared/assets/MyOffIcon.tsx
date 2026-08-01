f61fd7bcf8649f508e3757b89f3c47cb6f1bc39a
SleepingOff <justjh30@gmail.com>
2026-08-02 07:06:14 +0900

    ref: assets에서 svg를 컴포넌트로 변경

---
+++ src/shared/assets/MyOffIcon.tsx
@@ -0,0 +1,39 @@
+import Svg, { ClipPath, Defs, G, Path, Rect } from 'react-native-svg';
+
+import { TypeIconProps } from '../types/assets';
+
+export default function MyOffIcon({
+  size,
+  color = '#575757',
+}: TypeIconProps) {
+  return (
+    <Svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <G clipPath="url(#myOffClip)">
+        <Path
+          d="M12 13C14.7614 13 17 10.7614 17 8C17 5.23858 14.7614 3 12 3C9.23858 3 7 5.23858 7 8C7 10.7614 9.23858 13 12 13Z"
+          stroke={color}
+          strokeWidth={1.25}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <Path
+          d="M20 21C20 18.8783 19.1571 16.8434 17.6569 15.3431C16.1566 13.8429 14.1217 13 12 13C9.87827 13 7.84344 13.8429 6.34315 15.3431C4.84285 16.8434 4 18.8783 4 21"
+          stroke={color}
+          strokeWidth={1.25}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </G>
+      <Defs>
+        <ClipPath id="myOffClip">
+          <Rect width={24} height={24} fill="white" />
+        </ClipPath>
+      </Defs>
+    </Svg>
+  );
+}
